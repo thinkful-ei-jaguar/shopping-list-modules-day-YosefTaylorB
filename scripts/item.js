eb27620bb7f41b62c61da612ae36731ef9a4bd56
Taylor Bradshaw <taylordyoung1@gmail.com>
2019-12-09 15:42:50 -0500

    added validateName and create functions

--- scripts/item.js
+++ scripts/item.js
@@ -1,1 +1,20 @@
-export default {};
+function validateName (name) {
+    if (name === '') {
+        throw new TypeError ("Name must not be blank");
+    };
+}
+
+const create = function (name) {
+    let item = {
+        id: cuid(),
+        name: name,
+        checked: false,
+    };
+    
+    return item;
+}
+
+export default {
+    validateName,
+    create,
+};
